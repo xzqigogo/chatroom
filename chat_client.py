5706751d9d005e96742d14bf78404c632cef2054
xzqi <812373771@qq.com>
2021-07-08 22:02:17 +0800

    modify chatroom

--- chat_client.py
+++ chat_client.py
@@ -7,20 +7,86 @@
 @Desc  : 
 夏泽祺的程序
 '''
+import time
 from socket import *
+import pymysql
+import re
+
 
 class Client_serve(object):
     def __init__(self, ADDR, HOST):
         self.ADDR = ADDR
         self.HOST = HOST
-        self.ADDRESS = (self.ADDR, self.HOST)
-        self.sockfd=socket(AF_INET,SOCK_STREAM)
-        self.sockfd.connect(ADDR)
+        self.ADDRESS = ((self.ADDR, self.HOST))
+        self.sockfd = socket(AF_INET, SOCK_STREAM)
+        self.sockfd.connect(self.ADDRESS)
         self.main()
 
+    def chat_start(self):
+
+
     def main(self):
-        print("*********************************")
-        print("*******请输入你要做的操作************")
-        print("*******1.创建用户******************")
-        print("*******2.进入聊天室*****************")
-        print("**********************************")
+        while True:
+            print("*********************************")
+            print("*******请输入你要做的操作************")
+            print("*******1.创建用户******************")
+            print("*******2.进入聊天室*****************")
+            print("**********************************")
+            try:
+                option = int(input("请输入你要操作的选项"))
+            except Exception as e:
+                print('输入信息有误请重新输入')
+                continue
+            else:
+                if option not in (1, 2):
+                    print('输入信息有误请重新输入')
+                    continue
+                elif option == 1:
+                    while True:
+                        name = input("请输入你要创建的账号名称")
+                        if len(name) > 10:
+                            print("账号名称无法超过10个字符")
+                            continue
+                        while True:
+                            password = input("请输入密码")
+                            # print(re.findall('\w{6,15}', password))  #测试语句
+                            if re.findall('\w{6,15}', password)[0] != password:
+                                print("密码只能是大于6小于12的字母或者数字")
+                                continue
+                            break
+                        sqlword = 'insert into user_infor values("%s","%s")' % (name, password)
+                        self.sockfd.send(("L " + sqlword).encode())
+                        print("注册中，请稍等")
+                        data01 = self.sockfd.recv(128).decode()
+                        if data01 == 'ok':
+                            print("创建用户成功")
+                            break
+                        else:
+                            print("错误，请重新操作")
+                            break
+                else:
+                    while True:
+                        name = input('请输入用户账号:')
+                        password = input('请输入密码:')
+                        sqlword = 'select * from user_infor where name="%s"' % name
+                        self.sockfd.send(('I ' + sqlword).encode())
+                        time.sleep(0.1)
+                        data = self.sockfd.recv(128).decode()  # data是判断用户名是否存在的指标
+                        if data == 'no':
+                            print("用户名不存在")
+                            continue
+                        else:
+                            self.sockfd.send(password.encode())
+                            data = self.sockfd.recv(1024).decode()
+                            if data == 'ok':
+                                print('登录成功')
+                                self.chat_start()
+                            else:
+                                print("密码错误，请重新登录")
+                                continue
+
+
+if __name__ == '__main__':
+    host = '127.0.0.1'
+    port = 8888
+    c01 = Client_serve(host, port)
--- chat_server.py
+++ chat_server.py
@@ -8,8 +8,10 @@
 夏泽祺的程序
 '''
 import sys
+import time
 from socket import *
 import os
+import pymysql
 
 # 地址及端口号设置
 ADDR = '127.0.0.1'
@@ -23,28 +25,80 @@ class Chat_Serve(object):
     def __init__(self, ADDR, HOST):
         self.ADDR = ADDR
         self.HOST = HOST
-        self.ADDRESS = (self.ADDR, self.HOST)
+        self.ADDRESS = ((self.ADDR, self.HOST))
+        self.log=[]
         self.link_sockfd()
         self.main()
 
     def link_sockfd(self):
         self.sockfd = socket(AF_INET, SOCK_STREAM, proto=0)
+        self.sockfd.setsockopt(SOL_SOCKET, SO_REUSEPORT, 1)
         self.sockfd.bind(self.ADDRESS)
         self.sockfd.listen(5)
+
+    def connect_mysql(self, sql_language=None):
+        # connect database
+        db = pymysql.connect(user='root', password='123456',
+                             host='127.0.0.1', port=3306, database='chatroom', charset='utf8')
+        cur = db.cursor()
+        cur.execute(sql_language)
+        fetchone = cur.fetchall()
+        time.sleep(0.2)
+        db.commit()
+        cur.close()
+        db.close()
+        if not fetchone:
+            return
+        else:
+            return fetchone
+
+    def chat_start(self):
+        pass
+
 
     def manage(self):
         pass
 
     def connect_inter(self):
-        connfd, addr = self.sockfd.accept()
-        judge = connfd.recv(128).decode()#judge为判断用户输入的操作要求
-        if judge=="L "
+        self.connfd, addr = self.sockfd.accept()
+        self.log.append(self.connfd)
+        while True:
+            judge = self.connfd.recv(128).decode()  # judge为判断用户输入的操作要求
+            judgelist = judge.split(' ')
+            if judgelist[0] == "L":
+                print(' '.join(judgelist[1:]))  # 测试print
+                self.connect_mysql(' '.join(judgelist[1:]))
+                self.connfd.send(b'ok')
+            elif judgelist[0] == "I":
+                print(' '.join(judgelist[1:]))
+                user_info=self.connect_mysql(' '.join(judgelist[1:]))
+                # print(user_info)#测试语句，测试user_info的值
+                if not user_info:
+                    self.connfd.send(b'no')
+                else:
+                    self.connfd.send(b'ok')
+                    input_password=self.connfd.recv(1024).decode()
+                    print(input_password)
+                    if input_password==user_info[0][1]:
+                        self.connfd.send(b'ok')
+                        self.chat_start()
+                    else:
+                        self.connfd.send(b'wrong')
 
     def main(self):
         pid = os.fork()
         if pid < 0:
             sys.exit('end system....')
         elif pid == 0:
+            # print('进程1开始操作咯')
             self.manage()
+
         else:
+            # print('进程2开始操作咯')
             self.connect_inter()
+
+
+if __name__ == '__main__':
+    host = '127.0.0.1'
+    port = 8888
+    c01 = Chat_Serve(host, port)
